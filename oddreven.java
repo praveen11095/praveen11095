4e9a064a35f550da9919d568f496249cc2860f6c
praveen11095 <praveenkumar.spks1@gmail.com>
2016-11-04 20:42:16 +0530

    Update and rename posrneg.java to oddreven.java

---
+++ oddreven.java
@@ -0,0 +1,22 @@
+# praveen11095
+import java.util.Scanner;
+
+public class oddreven {
+
+	public static void main(String[] args) {
+		int n;
+		System.out.println("enter the number");
+		Scanner num = new Scanner(System.in);
+		n = num.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("the number is even");
+		}
+		else {
+			System.out.println("the number is odd");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
