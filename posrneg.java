3b376e65e71b9255e282580995577e7164ca3b52
praveen11095 <praveenkumar.spks1@gmail.com>
2016-11-04 20:08:15 +0530

    Create posrneg.java

---
+++ posrneg.java
@@ -0,0 +1,28 @@
+# praveen11095
+import java.util.*;
+public class posrneg {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner in = new Scanner(System.in);
+		n = in.nextInt();
+		System.out.println("enter the number"+n);
+		if(n==0)
+		{
+			System.out.println("the number is zero");
+		}
+		else 
+			if(n>0)
+		{
+			System.out.println("the number is positive");
+		}
+		else
+		{
+			System.out.println("the number is negative");
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
