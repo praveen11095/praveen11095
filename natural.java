6b077cfd9589f2f431800008cb5fc8d83b17cc0f
praveen11095 <praveenkumar.spks1@gmail.com>
2016-11-06 20:32:20 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class natural {
+
+	public static void main(String[] args) {
+		int n,i=0,sum=0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter the natural numbers to be added");
+	    n = in.nextInt();
+		{
+			for(i=0;i<=n;i++)
+				sum = sum +i;
+			{
+				System.out.println(" the value is  " +sum);
+			}
+		
+			
+			
+	
+		}
