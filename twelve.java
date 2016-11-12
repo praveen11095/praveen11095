b240b9d67577ed2f62a63419b3657b227e8003f7
praveen11095 <praveenkumar.spks1@gmail.com>
2016-11-12 10:58:07 +0530

    Create twelve.java

---
+++ twelve.java
@@ -0,0 +1,23 @@
+package praveen;
+
+import java.util.Scanner;
+
+public class twelve {
+
+	public static void main(String[] args) {
+		int a, b;
+		Scanner in = new Scanner(System.in);
+		a = in.nextInt();
+		System.out.println("enter the value 1  "   +a);
+		
+		b = in.nextInt();
+		System.out.println("enter the value 2  "   +b);
+		
+		System.out.println(Math.pow(a, b));
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
