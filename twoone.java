9ffd3840844cc62b8e0e3306b3aa53d8f43314e3
praveen11095 <praveenkumar.spks1@gmail.com>
2016-11-26 12:37:39 +0530

    Create twoone.java

---
+++ twoone.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class twoone {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n,i;
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter the multiplication number:");
+		n = in.nextInt();
+		for(i=0;i<=10;i++)
+		{
+			System.out.println("the value of +n +i  " +n*i);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
