2dec3783c65d2cd2503e01830fadb1da3aeb18e4
praveen11095 <praveenkumar.spks1@gmail.com>
2016-11-26 12:24:10 +0530

    Create fifteen.java

---
+++ fifteen.java
@@ -0,0 +1,15 @@
+
+public class fifteen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=0;i<=100;i++)
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+
+	}
+
+}
