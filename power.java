0e971abdad0ca510b58aa906fad0930af4af3f79
praveen11095 <praveenkumar.spks1@gmail.com>
2016-12-06 23:01:41 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class power {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+    Scanner in = new Scanner(System.in);
+    System.out.println("enter the number");
+    int i = in.nextInt();
+    System.out.println("enter the number 2");
+    int j = in.nextInt();
+    {
+    System.out.println(Math.pow(i,j));
+	}
+
+}
+}
