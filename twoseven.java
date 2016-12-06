14519a21da68e9d4decc7848a7f160f76f3caee9
praveen11095 <praveenkumar.spks1@gmail.com>
2016-12-06 22:40:26 +0530

    Create twoseven.java

---
+++ twoseven.java
@@ -0,0 +1,18 @@
+
+public class twoseven {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		String s = "praveen";
+
+for (int i = 0; i < s.length(); i++){
+    char c = s.charAt(i);        
+  
+System.out.println((int)c);
+
+}
+}
+}
